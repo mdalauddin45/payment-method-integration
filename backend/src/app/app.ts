5fa5a52647e3b1e407f72be4d85a775746a5d4c0
Eh-haque <ehtisamul@gmail.com>
2025-01-26 21:23:13 +0600

    deployment complete

--- backend/src/app/app.ts
+++ backend/src/app/app.ts
@@ -11,7 +11,12 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: "http://localhost:5174", credentials: true }));
+app.use(
+  cors({
+    origin: "https://payment-method-frontend.vercel.app",
+    credentials: true,
+  })
+);
 
 // application routes
 app.use("/api/v1", router);
